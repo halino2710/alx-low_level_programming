d2b791f588177050b241ea3f4ebb8ce9986a2dea
halino2710 <kemokaihamed2710@gmail.com>
2024-01-03 01:05:10 +0000

    Add Task 2: 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
- * Owned By Bwave
+ * _strlen - this function returns the length of a string
  * @s: char to check
- * Description: this will return the length of a string
  * Return: 0 is success
  */
 int _strlen(char *s)
